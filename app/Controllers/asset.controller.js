3ec048b440c7b4c57005ea0a23db136cc1c0254a
shaked <shakedbozi@gmail.com>
2021-06-01 23:54:58 +0300

    fix file name

--- app/Controllers/asset.controller.js
+++ app/Controllers/asset.controller.js
@@ -2,7 +2,7 @@
 const SimilarTechScanDomain = require('../Microservices/SimilarTech');
 const WhatCMSScanDomain = require('../Microservices/WhatCMS');
 const WappalyzerScanDomain = require('../Microservices/Wappalyzer');
-const { BatchOfQueriesModel } = require('../Schemas/BatchOfQueries.schemas');
+const { BatchOfQueriesModel } = require('../Schemas/batchOfQueries.schemas');
 
 // Constants
 const { R_IP } = require("../../client/src/Magic/Regex.magic");
