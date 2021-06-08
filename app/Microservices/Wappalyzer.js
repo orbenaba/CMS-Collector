8baece4b202223895571153c9dbe9a754645d6d1
shaked <shakedbozi@gmail.com>
2021-06-08 15:30:15 +0300

    test

--- app/Microservices/Wappalyzer.js
+++ app/Microservices/Wappalyzer.js
@@ -1,16 +1,16 @@
 const Wappalyzer = require("wappalyzer");
 const wappalyzer = new Wappalyzer();
+await wappalyzer.init();
 async function scanDomain(domain_ip) {
   try {
     let domain_ip_formatted = "https://" + domain_ip;
-    await wappalyzer.init();
 
     const results = await wappalyzer.open(domain_ip_formatted).analyze();
     return results.technologies;
   } catch (error) {
     console.error(error);
   }
-  await wappalyzer.destroy();
+  // await wappalyzer.destroy();
 }
 
 module.exports = scanDomain;
